4ada6a0cd5e17b9fa6076d90aa44e3e2ec172c2d
John Arevalo <johnarevalo@gmail.com>
2023-09-27 16:12:26 -0400

    Export external IDs

--- libs/jump_compound_annotator/src/jump/biokg.py
+++ libs/jump_compound_annotator/src/jump/biokg.py
@@ -17,7 +17,7 @@ def open_zip(output_path: Path, redownload=False):
         edges = pd.read_csv(fread,
                             sep='\t',
                             low_memory=False,
-                            names=['drugbank_id', 'rel_type', 'uniprot_id'])
+                            names=['source', 'rel_type', 'target'])
     return edges
 
 
@@ -25,15 +25,15 @@ def get_compound_annotations(output_dir: str):
     output_path = Path(output_dir)
     jump_ids = load_jump_ids(output_path).query('src_name=="drugbank"')
     edges = open_zip(output_path)
-    edges = edges.query('drugbank_id.isin(@jump_ids.src_compound_id)').copy()
-    uniprot_ids = edges['uniprot_id'].drop_duplicates().tolist()
+    edges = edges.query('source.isin(@jump_ids.src_compound_id)').copy()
+    uniprot_ids = edges['target'].drop_duplicates().tolist()
     results = get_gene_names(uniprot_ids)
     uniprot_to_gene = {r['from']: r['to'] for r in results['results']}
-    edges['target'] = edges['uniprot_id'].map(uniprot_to_gene)
+    edges['target'] = edges['target'].map(uniprot_to_gene)
     edges.dropna(subset=['target'], inplace=True)
     annotations = jump_ids.merge(edges,
                                  left_on='src_compound_id',
-                                 right_on='drugbank_id')
+                                 right_on='source')
     annotations = annotations.pivot_table(index='inchikey',
                                           columns='rel_type',
                                           values='target',
