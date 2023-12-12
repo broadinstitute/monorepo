c4097a3103ada46172408459a062d7f637b27649
Alán F. Muñoz <amunozgo@broadinstitute.org>
2023-12-12 17:11:33 -0500

    refactor(portrait): add get_gene_location_metadata

--- libs/jump_portrait/src/jump_portrait/fetch.py
+++ libs/jump_portrait/src/jump_portrait/fetch.py
@@ -10,12 +10,15 @@ from io import BytesIO
 import boto3
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pooch
 import requests
 from botocore import UNSIGNED
 from botocore.config import Config
 from s3path import S3Path
+
+S3_client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 
 METADATA_LOCATION = (
     "https://github.com/jump-cellpainting/datasets/raw/"
@@ -30,35 +33,39 @@ METAFILE_HASH = {
     "plate": "745391d930627474ec6e3083df8b5c108db30408c0d670cdabb3b79f66eaff48",
 }
 
-METADATA = {
-    _file: pd.read_csv(
-        pooch.retrieve(url=METADATA_LOCATION.format(_file), known_hash=_hash)
-    )
-    for _file, _hash in METAFILE_HASH.items()
-}
+METADATA = {}
 
 
-profile_formatter = (
-    "s3://cellpainting-gallery/cpg0016-jump/"
-    "{Metadata_Source}/workspace/profiles/"
-    "{Metadata_Batch}/{Metadata_Plate}/{Metadata_Plate}.parquet"
-)
+def get_table(table_name: str) -> pd.DataFrame:
+    if "table_name" not in METADATA:
+        METADATA[table_name] = pd.read_csv(
+            pooch.retrieve(
+                url=METADATA_LOCATION.format(table_name),
+                known_hash=METAFILE_HASH[table_name],
+            )
+        )
+    return METADATA[table_name]
+
 
 loaddata_formatter = (
     "s3://cellpainting-gallery/cpg0016-jump/"
     "{Metadata_Source}/workspace/load_data_csv/"
     "{Metadata_Batch}/{Metadata_Plate}/load_data_with_illum.parquet"
 )
-sample = (
-    METADATA["plate"]
-    .query('Metadata_PlateType=="TARGET2"')
-    .groupby("Metadata_Source")
-    .sample(2, random_state=42)
-)
-s3_path = loaddata_formatter.format(**sample.iloc[0].to_dict())
-parquet_meta = pd.read_parquet(s3_path, storage_options={"anon": True})
 
-S3_client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+
+def get_sample(n: int = 2, seed: int = 42):
+    sample = (
+        get_table("plate")
+        .query('Metadata_PlateType=="TARGET2"')
+        .groupby("Metadata_Source")
+        .sample(n, random_state=seed)
+    )
+
+    s3_path = loaddata_formatter.format(**sample.iloc[0].to_dict())
+
+    parquet_meta = pd.read_parquet(s3_path, storage_options={"anon": True})
+    return parquet_meta
 
 
 def build_s3_image_path(
@@ -74,7 +81,7 @@ def build_s3_image_path(
     )
 
 
-def get_image_from_s3path(s3_image_path):
+def get_image_from_s3path(s3_image_path: S3Path) -> np.array:
     response = s3_client.get_object(Bucket=s3_image_path.bucket, Key=s3_image_path.key)
     return mpimg.imread(BytesIO(response["Body"].read()), format="tiff")
 
@@ -87,7 +94,38 @@ def get_jump_image(
     site: int,
     channel: str,
     correction: str = None,
-):
+) -> np.ndarray:
+    """Main function to fetch a JUMP image for AWS.
+    Metadata for most files can be obtained from a set of data frames,
+    or generated using (TODO make metadata accessible) from this module.
+
+    Parameters
+    ----------
+    source : str
+        Which collaborator (data source) generated the images.
+    batch : str
+        Batch name.
+    plate : str
+        Plate name.
+    well : str
+        Well number (e.g., A01).
+    site : int
+        Site identifier (also called foci)
+    channel : str
+        Channel to fetch, the standard ones are DNA, Mito, ER and AGP.
+    correction : str
+        Whether or not to use corrected data. It does not by default.
+
+    Returns
+    -------
+    np.ndarray
+        Selected image as a numpy array.
+
+    Examples
+    --------
+    FIXME: Add docs.
+
+    """
     s3_location_frame_uri = loaddata_formatter.format(
         Metadata_Source=source, Metadata_Batch=batch, Metadata_Plate=plate
     )
@@ -103,3 +141,42 @@ def get_jump_image(
         row=unique_site.iloc[0], channel=channel, correction=correction
     )
     return get_image_from_s3path(s3_image_path)
+
+
+from broad_babel import query
+
+
+def get_gene_location_metadata(gene_name: str) -> pd.DataFrame:
+    """
+    First search for datasets in which this gene was present.
+    Return tuple with its Metadata location in order source, batch, plate,
+    well and site.
+    """
+
+    # Get plates
+    jcp_ids = query.run_query(
+        query=gene_name,
+        input_column="standard_key",
+        output_column="JCP2022,standard_key",
+    )
+    jcp_gene = {x[0]: x[1] for x in jcp_ids}
+    meta_wells = get_table("well")
+    found_rows = meta_wells[meta_wells["Metadata_JCP2022"].isin(jcp_gene.keys())].copy()
+    found_rows.loc[:, "gene"] = gene_name
+
+    # Get plate metadata with no reference to well and below
+    s3_path = loaddata_formatter.format(**sample.iloc[0].to_dict())
+    plate_level_metadata = get_table("plate").loc[
+        METADATA["plate"]["Metadata_Plate"].isin(found_rows["Metadata_Plate"])
+    ]
+    merged_metadata = pd.merge(
+        plate_level_metadata,
+        found_rows.drop("Metadata_Source", axis=1),
+        on="Metadata_Plate",
+    )
+    merged_metadata.columns = [
+        x.replace("Metadata_", "").lower() for x in merged_metadata.columns
+    ]
+    return merged_metadata
+
+    # parquet_meta = pd.read_parquet(s3_path, storage_options={"anon": True})
