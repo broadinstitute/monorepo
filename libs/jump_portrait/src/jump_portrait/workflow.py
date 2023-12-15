d0abc19f2000cf746d4a420ef7c3e1818f7c6806
Alán F. Muñoz <amunozgo@broadinstitute.org>
2023-12-14 19:45:59 -0500

    new(portrait): add workflow tmp file

---
+++ libs/jump_portrait/src/jump_portrait/workflow.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env jupyter
+
+
+"""
+Workflow 1: Fetch one image for a given item and a control
+"""
+from jump_portrait.fetch import get_jump_image, get_sample
+from jump_portrait.save import download_item_images
+
+sample = get_sample()
+
+source, batch, plate, well, site, *rest = sample.row(0)
+channel = "DNA"
+
+img = get_jump_image(source, batch, plate, well, channel, site)
+
+"""
+Workflow 2: Fetch all images for a given item and their controls
+"""
+
+item_name = "MYT1"  # Item or Compound of interest - (GC)OI
+channels = ["DNA"]  # Standard channels are ER, AGP, Mito DNA and RNA
+corrections = ["Orig"]  # Can also be "Illum"
+controls = True  # Fetch controls in plates alongside (GC)OI?
+
+download_item_images(item_name, channels, corrections=corrections, controls=controls)
