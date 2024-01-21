e42390f3f6fdb488d286dee3fc4fb61ac743e8f2
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-01-20 21:45:21 -0500

    refactor(rr): use cupy and speed up calculations

--- libs/jump_rr/src/calculate.py
+++ libs/jump_rr/src/calculate.py
@@ -28,52 +28,43 @@ import cupyx.scipy.spatial as spatial
 import numpy as np
 import polars as pl
 from broad_babel.query import run_query
-from utils import batch_processing, parallel
 
 assert cp.cuda.get_current_stream().done, "GPU not available"
 
-
-@batch_processing
-def jcp_to_standard(x):
-    return run_query(query=x, input_column="JCP2022", output_column="standard_key")[0][
-        0
-    ]
-
-
-@batch_processing
-def jcp_to_ncbi(x):
-    return run_query(query=x, input_column="JCP2022", output_column="NCBI_Gene_ID")[0][
-        0
-    ]
-
-
-# %%
-
-datasets_filenames = (
-    ("orf", "transformed_inf_eff_filtered"),
-    ("crispr", "harmonized_no_sphering_profiles"),
-)
-
+# %% Setup
+## Paths
 dir_path = Path("/dgx1nas1/storage/data/shared/morphmap_profiles/")
+datasets_filenames = (
+    ("crispr", "harmonized_no_sphering_profiles"),
+    ("orf", "transformed_inf_eff_filtered"),
+)
+output_dir = Path("./databases")
 
+## Parameters
+n_vals_used = 25  # Number of top and bottom matches used
+
+## Column names
+jcp_short = "JCP2022"  # Shortened input data frame
+jcp_col = f"Metadata_{jcp_short}"  # Traditional JUMP metadata colname
+url_col = "Metadata_image"  # Must start with "Metadata" for URL grouping to work
+match_col = "Match"  # Highest matches
+match_url_col = f"{match_col} Example"  # URL with image examples
+dist_col = "Distance"  # Metric name
+std_outname = "Gene/Compound"  # Standard item name
+ext_links_col = f"{match_col} resources"  # Link to external resources (e.g., NCBI)
+
+# HTML formatters
+external_formatter = (
+    '{{"href": "https://www.ncbi.nlm.nih.gov/gene/{}", "label":"External"}}'
+)
+url_template = (
+    '"https://phenaid.ardigen.com/static-jumpcpexplorer/' 'images/{}_{{}}.jpg"'
+)
+img_formatter = '{{"img_src": {}, "href": {}, "width": 200}}'
+
+# %% Processing starts
 for dataset, filename in datasets_filenames:
     profiles_path = dir_path / dataset / f"{filename}.parquet"
-    # %% Set names
-    jcp_col = "Metadata_JCP2022"  # Name of columns in input data frames
-    url_col = "Metadata_image"  # Must start with "Metadata"
-    match_jcp_col = "Match"
-    match_url_col = "Match Example"
-    dist_col = "Distance"
-    std_outname = "Gene/Compound"
-    ext_links_col = "Match resources"
-    ncbi_formatter = (
-        '{{"href": "https://www.ncbi.nlm.nih.gov/gene/{}", "label":"NCBI"}}'
-    )
-    url_template = (
-        '"https://phenaid.ardigen.com/static-jumpcpexplorer/' 'images/{}_{{}}.jpg"'
-    )
-    img_formatter = '{{"img_src": {}, "href": {}, "width": 200}}'
-    n_vals_used = 25
 
     # %% Load Metadata
     df = pl.read_parquet(profiles_path)
@@ -89,7 +80,7 @@ for dataset, filename in datasets_filenames:
         .map_elements(lambda x: url_template.format(x))
         .alias(url_col)
     )
-    grouped = df.group_by("Metadata_JCP2022")
+    grouped = df.group_by(jcp_col)
     med = grouped.median()
     meta = grouped.agg(pl.col("^Metadata_.*$").map_elements(cycle))
 
@@ -98,12 +89,9 @@ for dataset, filename in datasets_filenames:
     for srs in meta.iter_columns():
         med.replace_column(med.columns.index(srs.name), srs)
 
-    # vals = Tensor(med.select(pl.all().exclude("^Metadata.*$")).to_numpy())
     vals = cp.array(med.select(pl.all().exclude("^Metadata.*$")))
 
-    # %%
-
-    # Calculate cosine distance
+    # %% Calculate cosine distance
     cosine_sim = spatial.distance.cdist(vals, vals, metric="cosine")
 
     # Get most correlated and anticorrelated indices and values
@@ -113,51 +101,52 @@ for dataset, filename in datasets_filenames:
     indices = cosine_sim.argsort(axis=1)[:, mask].get()
     values = cosine_sim[cp.indices(indices.shape)[0], indices].get()
 
-    ## Build a dataframe containing matches
-    cycles = cycle(range(0 if dataset == "crispr" else 1, 9))
+    # Build a dataframe containing matches
+    cycles = cycle(range(dataset != "crispr", 10))  # 0-9 if CRISPR; 1-9 if ORF
     jcp_ids = urls.select(pl.col(jcp_col)).to_series().to_numpy().astype("<U15")
-    moving_idx = np.repeat(cycles, len(vals))
-    urls_vals = urls.get_column(url_col).to_numpy()
+    moving_idx = np.repeat(cycles, len(indices))
+    url_vals = urls.get_column(url_col).to_numpy()
 
     jcp_df = pl.DataFrame(
         {
-            "JCP2022": np.repeat(jcp_ids, n_vals_used * 2),
-            match_jcp_col: jcp_ids[indices.flatten()].astype("<U15"),
+            jcp_short: np.repeat(jcp_ids, n_vals_used * 2),
+            match_col: jcp_ids[indices.flatten()].astype("<U15"),
             dist_col: values.flatten(),
-            url_col: [
+            url_col: [  # Secuentially produce multiple images
                 img_formatter.format(img_src, img_src)
-                for x in urls.get_column(url_col).to_numpy()
+                for x in url_vals
                 for j in range(n_vals_used * 2)
-                if (img_src := next(x).format(j % 9))
+                if (img_src := next(x).format(next(cycles)))
             ],
-            match_url_col: [
+            match_url_col: [  # Use indices to fetch matches
                 img_formatter.format(img_src, img_src)
                 for url, idx in zip(
-                    urls_vals[indices.flatten()], moving_idx[indices.flatten()]
+                    url_vals[indices.flatten()], moving_idx[indices.flatten()]
                 )
                 if (img_src := next(url).format(next(idx)))
             ],
         }
     )
 
-    # jcp_df.write_parquet(checkpoint_path, compression="zstd")
+    # %% Add gene name translations
+    uniq_jcp = tuple(jcp_df.unique(subset=jcp_short).to_numpy()[:, 0])  # [0]
+    mapper_values = run_query(
+        query=uniq_jcp,
+        input_column=jcp_short,
+        output_column=f"{jcp_short},standard_key,NCBI_Gene_ID",
+        predicate=f"AND plate_type = '{dataset}'",
+    )
 
-    # %% And then proceed to add gene names
-
-    uniq_jcp = jcp_df.select(pl.col("JCP2022")).to_series().unique().to_list()
-    mapper_values = parallel(uniq_jcp, jcp_to_standard)
-    mapper = {jcp: std for jcp, std in zip(uniq_jcp, mapper_values)}
-
-    ncbi_mapper_values = parallel(uniq_jcp, jcp_to_ncbi)
-    ncbi_mapper = {
-        jcp: ncbi_formatter.format(idx)
-        for jcp, idx in zip(uniq_jcp, ncbi_mapper_values)
-    }
+    jcp_external_mapper = {}
+    jcp_std_mapper = {}
+    for jcp, std, external_id in mapper_values:
+        jcp_external_mapper[jcp] = external_formatter.format(external_id)
+        jcp_std_mapper[jcp] = std
 
     jcp_translated = jcp_df.with_columns(
-        pl.col("JCP2022").replace(mapper).alias(std_outname),
-        pl.col(match_jcp_col).replace(mapper),
-        pl.col(match_jcp_col).replace(ncbi_mapper).alias(ext_links_col),
+        pl.col(jcp_short).replace(jcp_std_mapper).alias(std_outname),
+        pl.col(match_col).replace(jcp_std_mapper),
+        pl.col(match_col).replace(jcp_external_mapper).alias(ext_links_col),
     )
 
     matches = jcp_translated.with_columns(
@@ -167,53 +156,21 @@ for dataset, filename in datasets_filenames:
     # Sort columns
     order = [
         std_outname,
-        "Match",
-        "Gene/Compound Example",
+        match_col,
+        f"{std_outname} Example",
         match_url_col,
         dist_col,
         ext_links_col,
-        "JCP2022",
+        jcp_short,
     ]
     matches_translated = matches.select(order)
 
-    final_output = f"{dataset}.parquet"
+    # %% Save results
+    output_dir.mkdir(parents=True, exist_ok=True)
+    final_output = output_dir / f"{dataset}.parquet"
     matches_translated.write_parquet(final_output, compression="zstd")
 
     """
-
-    # %% Upload to Zenodo
-    # Automated uploads are not working
-    # https://github.com/zenodo/zenodo/issues/2506
-
-    from os.path import expanduser
-    import requests
-    with open(expanduser("~") + "/.zenodo") as f:
-        for line in f.readlines():
-            name, access_token = line.strip("\n").split(": ")
-
-    params = {"access_token": access_token}
-    bucket_url = "https://zenodo.org/api/files/9ffb1b5b-1e3c-4618-ad91-36255e87b57e"
-    headers = {"Content-Type": "application/json"}
-    with open(final_output, "rb") as fp:
-        r = requests.post(
-            f"{bucket_url}?access_token={params['access_token']}",
-            data=fp,
-            headers=headers,
-            params=params,
-        )
-
-
-    depos_url =  "https://zenodo.org/api/deposit/depositions/10416605"
-    depos=requests.get(depos_url,params=params)
-    new = requests.post(depos.json()["links"]["newversion"], params=params, headers={"Accept": "application/json" ,**headers})
-
-    d = r.post(depos_url.json(""))
-    curl -X POST-H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer <TOKEN>" https://sandbox.zenodo.org/api/deposit/depositions/165/actions/newversion
-
-    """
-
-    """
-    Future plans:
-    - TODO add differentiating features when compared to their controls
     - TODO add Average precision metrics
+    - STOP add differentiating features when compared to their controls
     """
