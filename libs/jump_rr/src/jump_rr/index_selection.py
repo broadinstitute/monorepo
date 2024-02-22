84729067c6fd4f1df7adfc7ad52101f327fb543e
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-02-21 20:35:18 -0500

    feat(rr): add edge indices

--- libs/jump_rr/src/jump_rr/index_selection.py
+++ libs/jump_rr/src/jump_rr/index_selection.py
@@ -15,3 +15,21 @@ def get_bottom_top_indices(mat: cp.array, n: int, skip_first=False) -> cp.array:
     xs = cp.indices(indices.shape)[0].flatten().get()
     ys = indices.flatten().get()
     return xs, ys
+
+
+def get_edge_indices(mat: cp.array, n: int, which: str = "bottom") -> cp.array:
+    # Get the top n and bottom n indices from a matrix for each row.
+    mask = cp.ones(mat.shape[1], dtype=bool)
+
+    assert which in ("top", "bottom"), "which must be either top or bottom"
+
+    if which == "bottom":
+        mask[n - 1 :] = False
+    else:
+        mask[: -n - 1] = False
+
+    indices = mat.argsort(axis=1)[:, mask]
+
+    xs = cp.indices(indices.shape)[0].flatten().get()
+    ys = indices.flatten().get()
+    return xs, ys
