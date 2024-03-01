f502f03978f15c1b1ede6b6d26a8f53c72845b3c
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-02-29 19:53:26 -0500

    change(kaljax): add first working example

--- libs/kaljax/src/kaljax/kalman.py
+++ libs/kaljax/src/kaljax/kalman.py
@@ -1,2 +1,69 @@
 #!/usr/bin/env jupyter
+import jax.numpy as jnp
+from jax.random import PRNGKey, uniform
 
+key = PRNGKey(1)
+n_iter = 50
+dt = 0.1
+n_observed = 2
+
+# "Observations"
+
+X = jnp.array([[0], [0], [0.1], [0.1]])
+Ys = jnp.cumsum(uniform(key, (n_iter, n_observed)), axis=0)
+P = jnp.eye(len(X)) * 0.1
+A = jnp.array(
+    [
+        [1, 0, dt, 0],
+        [0, 1, 0, dt],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ]
+)
+# Measurement matrices
+
+# Constants
+Q = jnp.eye(len(X))
+B = jnp.eye(len(X))
+U = jnp.zeros((len(X), 1))
+H = jnp.array(
+    [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+    ]
+)
+R = jnp.eye(Ys.shape[1])
+
+
+def generate_initial_state() -> jnp.ndarray:
+    X = jnp.array([[0.0], [0.0], [0.1], [0.1]])
+    return X
+
+
+def kf_predict(X, P, A, Q, B, U):
+    X = jnp.dot(A, X) + jnp.dot(B, U)
+    P = jnp.dot(A, jnp.dot(P, A.T)) + Q
+
+    return X, P
+
+
+def kf_update(X, P, Y, H, R):
+    IM = jnp.dot(H, X)
+    IS = R + jnp.dot(H, jnp.dot(P, H.T))
+    K = jnp.dot(P, jnp.dot(H.T, jnp.linalg.inv(IS)))
+    X = X + jnp.dot(K, (Y - IM))
+    P = P - jnp.dot(K, jnp.dot(IS, K.T))
+    return (X, P, K, IM, IS)
+
+
+def apply_kalman_iteration(X, P, K, IM, IS, Y):
+    X, P = kf_predict(X, P, A, Q, B, U)
+    X, P, K, IM, IS = kf_update(X, P, Y, H, R)
+    return X, P, K, IM, IS
+
+
+a = []
+for Y in Ys:
+    X, P = kf_predict(X, P, A, Q, B, U)
+    X, P, K, IM, IS = kf_update(X, P, Y, H, R)
+    a.append(X)
