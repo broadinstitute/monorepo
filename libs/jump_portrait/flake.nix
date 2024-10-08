4f15c3adba5203ae277b5ae14f3c0f1ded5b2fb4
Alán F. Muñoz <afer.mg@gmail.com>
2024-10-08 16:56:42 -0400

    deps(portrait): update nix and pyproject deps

--- libs/jump_portrait/flake.nix
+++ libs/jump_portrait/flake.nix
@@ -1,62 +1,66 @@
 {
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.follows = "dream2nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, dream2nix, ... } @ inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-        pkgs = import nixpkgs {
-          system = system;
-          config.allowUnfree = true;
-        };
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
 
-        mpkgs = import inputs.nixpkgs_master {
-          system = system;
-          config.allowUnfree = true;
-        };
-
-      in  {
-        devShells =
+      devShells = forEachSystem
+        (system:
           let
-            python_with_pkgs = (pkgs.python310.withPackages(pp: []));
-          in
-            with pkgs;
-            {
-              default = pkgs.mkShell {
-                NIX_LD = runCommand "ld.so" {} ''
-                  ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
-                '';
-                NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-                  pkgs.zlib
-                ];
-                packages = [
-                  mpkgs.rye
-                  uv
-                ];
-                venvDir = "./.venv";
-                postVenvCreation = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                postShellHook = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                shellHook = ''
-                  export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:"${pkgs.stdenv.cc.cc.lib}/lib"
-                  export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
-                  runHook venvShellHook
-                  export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
-#                  rye sync
-#                  source .venv/bin/activate
-              '';
-              };
+            pkgs = import nixpkgs {
+              system = system;
+              config.allowUnfree = true;
             };
-      }
-    );
+
+            mpkgs = import inputs.nixpkgs_master {
+              system = system;
+              config.allowUnfree = true;
+            };
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  stdenv = pkgs.clangStdenv;
+                  env.NIX_LD = nixpkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+                  env.NIX_LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath (with pkgs; [
+                  # Add needed packages here
+                  pkgs.libz # for numpy
+                  pkgs.stdenv.cc.cc
+                  pkgs.libGL
+                  ]);
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    poetry
+                  ];
+                  enterShell = ''
+                    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+                    export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+                    if [ ! -d ".venv" ]; then
+                       poetry install -vv --with dev
+                    fi
+                    source .venv/bin/activate
+                  '';
+                }
+              ];
+            };
+          });
+    };
 }
