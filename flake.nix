cb63f9409b1e7eb4cb2692d78ecfd977c1b4021e
Alán F. Muñoz <afer.mg@gmail.com>
2024-12-12 17:12:07 -0500

    [Nix Flake Update]: Updated dependencies and systems configurations

--- flake.nix
+++ flake.nix
@@ -6,10 +6,27 @@
     flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs, flake-utils, systems, ... } @ inputs:
-      flake-utils.lib.eachDefaultSystem (system:
-        {
-          templates = import ./management/project_templates;
-        }
-      );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      systems,
+      ...
+    }@inputs:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
+      in
+      { }
+
+    )
+    // {
+      templates = import ./management/project_templates { };
+    };
 }
