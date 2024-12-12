27aace3acc5eef0ca0ef7b4ab52ba73900fa187c
Alán F. Muñoz <afer.mg@gmail.com>
2024-12-12 16:32:32 -0500

    feat: Add GPU-supported project templates for Torch and Txtai

---
+++ management/project_templates/default.nix
@@ -0,0 +1,11 @@
+{ }:
+{
+  torch_gpu = {
+    path = ./torch_pure;
+    description = "Pytorch environment with GPU support.";
+  };
+  txtai_gpu = {
+    path = ./txtai_gpu;
+    description = "Txtai (RAG) environment with GPU support.";
+  };
+}
