606f26ed0dc84f27df70f8f2b1ef02e88466bf30
Alan Munoz <afer.mg@gmail.com>
2025-03-31 00:09:38 -0400

    chore(rr): finish uv migration

--- libs/jump_rr/flake.nix
+++ libs/jump_rr/flake.nix
@@ -3,70 +3,91 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
+  outputs =
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.cudaSupport = true;
+        };
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = import nixpkgs {
-              system = system;
-              config.allowUnfree = true;
-            };
-            mpkgs = import inputs.nixpkgs_master {
-              system = system;
-              config.allowUnfree = true;
-            };
+        mpkgs = import inputs.nixpkgs_master {
+          inherit system;
+          config.allowUnfree = true;
+          config.cudaSupport = true;
+        };
 
-          in
-            {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  stdenv = pkgs.clangStdenv;
-                  env.NIX_LD = nixpkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-                  env.NIX_LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath (with pkgs; [
-                  # Add needed packages here
-                  cudaPackages.cudatoolkit
-                  #linuxPackages.nvidia_x11
-                  pkgs.libz # for numpy
-                  mpkgs.stdenv.cc.cc
-                  pkgs.libGL
-                  ]);
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [
-                    poetry
-                    python311Packages.cupy
-                    csvtool
-                  ];
-                  enterShell = ''
-                    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:${pkgs.cudatoolkit}
-                    # export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+        libList =
+          [
+            # Add needed packages here
+            mpkgs.stdenv.cc.cc
+            mpkgs.libGL
+            mpkgs.glib
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux (
+            with pkgs;
+            [
+              cudatoolkit
 
-                    export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
-                    if [ ! -d ".venv" ]; then
-                       poetry install -vv --with dev
-                    fi
-                    source .venv/bin/activate
-                  '';
-                }
-              ];
+              # This is required for most app that uses graphics api
+              # linuxPackages.nvidia_x11
+            ]
+          );
+      in
+      with pkgs;
+      {
+        devShells = {
+          default =
+            let
+              python_with_pkgs = pkgs.python311.withPackages (pp: [
+                # Add python pkgs here that you need from nix repos
+                python311Packages.cupy
+              ]);
+            in
+            mkShell {
+              NIX_LD = runCommand "ld.so" { } ''
+                ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+              '';
+              NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
+              packages = [
+                python_with_pkgs
+                python311Packages.venvShellHook
+                # We # We now recommend to use uv for package management inside nix env
+                mpkgs.uv
+              ] ++ libList;
+              venvDir = "./.venv";
+              postVenvCreation = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              postShellHook = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              shellHook = ''
+                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:"/run/opengl-driver/lib":$LD_LIBRARY_PATH
+                export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+                export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+                runHook venvShellHook
+                export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+              '';
             };
-          });
-    };
+        };
+      }
+    );
 }
+# Things one might need for debugging or adding compatibility
+# export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+# export LD_LIBRARY_PATH=${pkgs.cudaPackages.cuda_nvrtc}/lib
+# export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+# export EXTRA_CCFLAGS="-I/usr/include"
