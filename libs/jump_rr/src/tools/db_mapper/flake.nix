2d4c7584873fbbd3525558e3ef9f8b786ce7de5d
Alan Munoz <afer.mg@gmail.com>
2025-06-13 15:11:04 -0400

    feat(rr): add db_mapper flake

---
+++ libs/jump_rr/src/tools/db_mapper/flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs_master.url = "github:NixOS/nixpkgs/master";
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+      in
+      with pkgs;
+      {
+        devShells = {
+          default = mkShell {
+            packages = [
+              wget
+              coreutils
+              parallel
+              mawk
+              pzip
+              duckdb
+            ];
+          };
+        };
+      }
+    );
+}
