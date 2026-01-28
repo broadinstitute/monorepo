8c6e04c553861744a355a3d9b6c4dcbe7aa7e399
Shantanu Singh <shsingh@broadinstitute.org>
2026-01-27 20:25:39 -0500

    chore(copairs_runner): switch copairs dependency to main branch
    The hierarchical-fdr branch has been merged into main.

--- libs/copairs_runner/src/copairs_runner/copairs_runner.py
+++ libs/copairs_runner/src/copairs_runner/copairs_runner.py
@@ -3,7 +3,7 @@
 # dependencies = [
 #     "pandas",
 #     "numpy",
-#     "copairs @ git+https://github.com/cytomining/copairs.git@hierarchical-fdr",
+#     "copairs @ git+https://github.com/cytomining/copairs.git@main",
 #     "omegaconf",
 #     "hydra-core",
 #     "pyarrow",
