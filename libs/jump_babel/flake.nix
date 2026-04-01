4bb28434879bec616a0abdbfc96d33f736e0e46f
Alán F. Muñoz <amunozgo@broadinstitute.org>
2026-04-01 13:39:03 -0400

    feat(jump_babel): Add linting and update Python environment
    Integrate treefmt and git-hooks for automated formatting and
    pre-commit checks. Update the development shell to use Python 3.13
    and improve uv integration.

--- libs/jump_babel/flake.nix
+++ libs/jump_babel/flake.nix
@@ -5,6 +5,14 @@
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -12,6 +20,8 @@
       self,
       nixpkgs,
       flake-utils,
+      git-hooks,
+      treefmt-nix,
       ...
     }@inputs:
     flake-utils.lib.eachDefaultSystem (
@@ -22,23 +32,49 @@
           config.allowUnfree = true;
         };
 
-        libList =
-          [
-            # Add needed packages here
-            pkgs.stdenv.cc.cc
-            pkgs.libGL
-            pkgs.glib
-            pkgs.libz
-          ];
+        libList = [
+          # Add needed packages here
+          pkgs.stdenv.cc.cc
+          pkgs.libGL
+          pkgs.glib
+          pkgs.libz
+        ];
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.ruff-format.enable = true;
+          programs.ruff-check.enable = true;
+        };
+
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          package = pkgs.prek;
+          hooks = {
+            treefmt = {
+              enable = true;
+              package = treefmtEval.config.build.wrapper;
+            };
+          };
+        };
       in
       with pkgs;
       {
+        checks = {
+          inherit pre-commit-check;
+          formatting = treefmtEval.config.build.check self;
+        };
+        formatter = treefmtEval.config.build.wrapper;
         devShells = {
           default =
             let
-              python_with_pkgs = pkgs.python310.withPackages (pp: [
-                # Add python pkgs here that you need from nix repos
-              ]);
+              pwp = (
+                python313.withPackages (
+                  p: with p; [
+                    venvShellHook
+                  ]
+                )
+              );
             in
             mkShell {
               NIX_LD = runCommand "ld.so" { } ''
@@ -46,11 +82,12 @@
               '';
               NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
               packages = [
-                python_with_pkgs
-                python310Packages.venvShellHook
+                pwp
+                python312Packages.venvShellHook
                 # We # We now recommend to use uv for package management inside nix env
                 pkgs.uv
-              ] ++ libList;
+              ]
+              ++ libList;
               venvDir = "./.venv";
               postVenvCreation = ''
                 unset SOURCE_DATE_EPOCH
@@ -59,18 +96,18 @@
                 unset SOURCE_DATE_EPOCH
               '';
               shellHook = ''
-                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:"/run/opengl-driver/lib":$LD_LIBRARY_PATH
+                ${pre-commit-check.shellHook}
+                export UV_PYTHON=${pkgs.python313}
+                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
                 export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+
+                uv sync --all-groups
+                export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
                 runHook venvShellHook
-                export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+                source .venv/bin/activate
               '';
             };
         };
       }
     );
 }
-# Things one might need for debugging or adding compatibility
-# export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-# export LD_LIBRARY_PATH=${pkgs.cudaPackages.cuda_nvrtc}/lib
-# export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-# export EXTRA_CCFLAGS="-I/usr/include"
