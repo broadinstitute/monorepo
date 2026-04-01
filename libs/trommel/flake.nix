24bb2367d443bd3486dd1a07ff1e5b0e015eebeb
Alán F. Muñoz <amunozgo@broadinstitute.org>
2026-04-01 16:19:38 -0400

    ci: add workflows and update trommel development environment
    - Add GitHub Actions for linting (Nix) and testing (uv) Update
    - trommel Nix development shell to Python 3.13 Integrate treefmt and
    - pre-commit hooks into trommel's flake Expand Python version
    - support in pyproject.toml Add ipdb, jupyter, and pytest to
    - trommel's dependency groups Fix return value handling in
    - integration tests

--- libs/trommel/flake.nix
+++ libs/trommel/flake.nix
@@ -5,6 +5,14 @@
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,6 +21,8 @@
       nixpkgs,
       flake-utils,
       systems,
+      git-hooks,
+      treefmt-nix,
       ...
     }@inputs:
     flake-utils.lib.eachDefaultSystem (
@@ -35,15 +45,38 @@
           pkgs.libGL
           pkgs.glib
         ];
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.ruff-format.enable = true;
+          programs.ruff-check.enable = true;
+        };
+
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          package = pkgs.prek;
+          hooks = {
+            treefmt = {
+              enable = true;
+              package = treefmtEval.config.build.wrapper;
+            };
+          };
+        };
       in
       with pkgs;
       {
+        checks = {
+          inherit pre-commit-check;
+          formatting = treefmtEval.config.build.check self;
+        };
+        formatter = treefmtEval.config.build.wrapper;
         devShells = {
           default =
             let
               # These packages get built by Nix, and will be ahead on the PATH
               pwp = (
-                python311.withPackages (
+                python313.withPackages (
                   p: with p; [
                     python-lsp-server
                     python-lsp-ruff
@@ -58,7 +91,7 @@
               NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
               packages = [
                 pwp
-                python311Packages.venvShellHook
+                python313Packages.venvShellHook
                 uv
 
                 ruff
@@ -72,14 +105,14 @@
                 unset SOURCE_DATE_EPOCH
               '';
               shellHook = ''
-                export UV_PYTHON=${pkgs.python311}
+                export UV_PYTHON=${pkgs.python313}
                 export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
                 export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
 
-                uv sync --group dev
-
-                runHook venvShellHook
+                uv sync --all-groups
                 export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
+                runHook venvShellHook
+                source .venv/bin/activate
               '';
             };
         };
